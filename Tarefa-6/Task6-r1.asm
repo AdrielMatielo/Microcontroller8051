7e34db54b3ac19bc80e78e8ab077b29cbfb6b5c6
AdrielMatielo <adrielcmmatielo@gmail.com>
2023-08-04 19:51:37 -0300

    Organization

--- Tarefa-6/Task6-r1.asm
+++ Tarefa-6/Task6-r1.asm
@@ -19,7 +19,7 @@
 				$mod51
 
 				org 0000h	; RESET	
-					ljmp KICK
+					ljmp INIT
 
 				org 0003h	; /INT0
 					ljmp ISR_EX0
@@ -38,104 +38,85 @@
 					
 									
 				STOP equ 0028h
-				UNITS equ R4
-				TENS equ R5
-				HUNDREDS equ R6
-				THOUSANDS equ R7
+				
 
 
 
-KICK:			mov IE, #1000$1111b      			; Interruption Register: (1)EA -(X)-(X)-(0)E/S- $ -(1)Timer1-(1)Ex1-(1)Timer0-(1)Ex0
+INIT:			mov IE, #1000$1111b      			; Interruption Register: (1)EA -(X)-(X)-(0)E/S- $ -(1)Timer1-(1)Ex1-(1)Timer0-(1)Ex0
 				mov TMOD, #0001$0001b				; Timer/Counter Register: T1 (0)TR1-(0)Timer-(01)Mode01 -$- T2 (0)TR2-(0)Timer-(01)Mode01
 				
-				mov TH0, #006h						;timer for multiplexer
-				mov TL0, #0F3h
-				;mov R0, 10
+				mov TH0, #0D1h						;timer for multplexer
+				mov TL0, #083h
+				;mov R0, 10							;Repet Counter R0 times
 				
-				mov TH1, #006h						;Timer for counting
-				mov TL1, #0F3h
+				mov TH1, #0D1h						;Timer for counting
+				mov TL1, #083h
 				mov R1, 10							;Repet Counter R1 times
 				
 				setb STOP
 				
 				mov R3, #0				; Inicializa o iterador da lista (aqui vamos usar o registrador R3).
+				mov DPTR, #NUMBERS0		; Carrega a lista de números no data pointer.
 				
 				setb EA
 				setb TR0
-				setb TR1
-				
-				mov UNITS, 0
-				mov TENS, 0
-				mov HUNDREDS, 0
-				mov THOUSANDS, 0
 				
 				
 				sjmp START				; Pula para o laço principal
 
 START:				
-				lcall DISPLAY
-				jnb STOP, KICK
-				jnb TF1, START
-				clr TF1
-				lcall SUM
-				sjmp START				; e retorna ao início do laço neste ponto.
-				
-CONTINUE:		inc R3					; Se a lista não chegou ao fim, incrementa o iterador
-				sjmp START	
-	
-SUM:			cjne UNITS, #010, SUM_UNITS
-				cjne TENS, #010, END_COUNTER
-				cjne HUNDREDS, #010, END_COUNTER
-				cjne THOUSANDS, #010, END_COUNTER
-				mov UNITS, #0
-				mov TENS, #0
-				mov HUNDREDS, #0
-				mov THOUSANDS, #0
-				sjmp END_COUNTER
-				SUM_UNITS:		inc UNITS
-								ret
-				SUM_TENS:		inc TENS
-								ret
-				SUM_HUNDREDS: 	inc HUNDREDS
-								ret
-				SUM_THOUSANDS: 	inc THOUSANDS
-								ret
-END_COUNTER:	ret
-; ======================================= ROUTINES
-
-NUMBERS0: db 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
-	
-MULTIPLEX0:		clr P3.0
-				clr P3.1
-				mov P2, UNITS
-				ANL P2, #0Fh
-				ANL A, #0F0h
-				ORL P2, A
-				ret
-
-MULTIPLEX1:		setb P3.0
-				clr P3.1
-				mov P2, TENS
-				ret
-				
-MULTIPLEX2:		clr P3.0
-				setb P3.1
-				mov P2, HUNDREDS
-				ret
-				
-MULTIPLEX3:		setb P3.0
-				setb P3.1
-				mov P2, THOUSANDS
-				ret
-				
-DISPLAY:	lcall MULTIPLEX0
+			
+				lcall MULTIPLEX0
 				jnb TF0, $	
 				lcall MULTIPLEX1
 				jnb TF0, $	
 				lcall MULTIPLEX2
 				jnb TF0, $	
 				lcall MULTIPLEX3
-				jnb TF0, $
+				jnb TF0, $				;espera a contagem terminar
+				jnb TR1, START
+				jnb STOP, INIT
+				mov A, R3
+				movc A, @A+DPTR
+				cjne A, #010, CONTINUE	; Compara pra verificar se a lista não chegou ao fim (número 9).
+				mov R3, #0				; Se a lista chegou ao fim, zera o iterador (pra começar do início novamente		
+				sjmp START				; e retorna ao início do laço neste ponto.
+				
+CONTINUE:		inc R3					; Se a lista não chegou ao fim, incrementa o iterador
+				sjmp START	
+	
+	
+; ======================================= ROUTINES
+
+NUMBERS0: db 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+;NUMBERS1: db 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+;NUMBERS2: db 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+;NUMBERS3: db 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+
+
+SHOWDISPLAY:	anl A, #0x0F
+				mov P2, A
+				anl A, #0x0F
+				ret
+				
+MULTIPLEX0:		clr P3.0
+				clr P3.1
+				lcall SHOWDISPLAY
+				ret
+
+MULTIPLEX1:		setb P3.0
+				clr P3.1
+				lcall SHOWDISPLAY
+				ret
+				
+MULTIPLEX2:		clr P3.0
+				setb P3.1
+				lcall SHOWDISPLAY
+				ret
+				
+MULTIPLEX3:		setb P3.0
+				setb P3.1
+				lcall SHOWDISPLAY
 				ret
 				
 ; ======================================= INTERRUPTIONS
@@ -148,8 +129,8 @@ ISR_EX0:		jnb P3.2, $						; Wait until release button.
 ISR_TIMER0:			
 				clr EA				; Clear interruption
 				;djnz R0, ENDTIMER0					; If TF0 interrupts R0 times, Flag gets 0 and stop TR0
-				mov TH0, #006h						;
-				mov TL0, #0F3h						;
+				mov TH0, #0E8h						;
+				mov TL0, #08Fh						;
 				;mov R0, 10
 ENDTIMER0:		clr TF0
 				setb EA
@@ -162,11 +143,12 @@ ISR_EX1:		jnb P3.3, $
 ISR_TIMER1:			
 				clr EA				; Clear interruption
 				djnz R1, ENDTIMER1					; If TF0 interrupts R0 times, Flag gets 0 and stop TR0
-				mov TH1, #006h						; 3C is the high part of (FF00 - 50ms/1us)  
-				mov TL1, #0F3h						; AF is the low part of (FFFF - 50ms/1us)
-				mov TH0, #006h						;
-				mov TL0, #0F3h
-ENDTIMER1:		
+				mov TH1, #0A2h						; 3C is the high part of (FF00 - 50ms/1us)  
+				mov TL1, #03Fh						; AF is the low part of (FFFF - 50ms/1us)
+				mov TH0, #0E8h						;
+				mov TL0, #08Fh						;
+				mov R1, 10
+ENDTIMER1:		clr TF1
 				setb EA
 				reti
 			
